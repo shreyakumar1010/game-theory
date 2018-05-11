dca28858f6eef0d7cfbd4e55576572439366d8c7
shreyakumar1010 <shreyakumar.sk@gmail.com>
2018-05-11 01:43:40 -0500

    Game of Trust additions

--- js/app3.js
+++ js/app3.js
@@ -43,11 +43,11 @@ function showScores() {
 
 // create questions
 var questions = [
-    new Question("Which one is not an object oriented programming language?", ["Java", "C#","C++", "C"], "C"),
-    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
-    new Question("There are ____ main components of object oriented programming.", ["1", "6","2", "4"], "4"),
-    new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
-    new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework")
+    new Question("What should be the strategy in the first round?", ["Cheat", "Co-operate","Quit", "Other"], "Co-operate"),
+    new Question("What is the lowest possible outcome in 3 rounds?", ["0", "-1", "-3", "1"], "-3"),
+    new Question("What should be the strategy in the last round?", ["Cheat", "Co-operate","Quit", "Other"], "Cheat"),
+    new Question("What is another name for the game of trust?", ["Iterated Prisoner", "Chicken", "Trust Me", "Prisoner"], "Iterated Prisoner"),
+    new Question("What is the highest possible outcome in 3 rounds?", ["4", "6", "7", "8"], "7")
 ];
 
 // create quiz
